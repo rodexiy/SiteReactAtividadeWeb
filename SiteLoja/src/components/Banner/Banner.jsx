beed0b30b1fc0aedb0a1cac4d59f50a40963fea7
rodexiy <rodexcontato@gmail.com>
2023-10-11 21:48:06 -0300

    j

--- SiteLoja/src/components/Banner/Banner.jsx
+++ SiteLoja/src/components/Banner/Banner.jsx
@@ -3,10 +3,27 @@ import "./Banner.css"
 function Banner() {
     return (
         <>
-            <div className="main">
-                <img src="images/banner.jpg"></img>
-            </div>
-        
+            <section className="banner_main">
+                <div className="banner_card">
+                    <div className="banner_texts">
+                        <h4 className="banner_top">
+                            New Arrival
+                        </h4>
+                        <h3 className="banner_titulo">
+                            Discover Our New Collection
+                        </h3>
+
+                        <p className="banner_paragrafo">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.
+                        </p>
+
+                        <a className="banner_button">
+                            BUY NOW
+                        </a>
+                    </div>
+                </div>
+            </section>
+
         </>
     )
 }
